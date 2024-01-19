6c780e1fad440260f207144770f822a625af7bec
UJJWAL MANGAL ATREYA <ujjwalmangalatreya@UJJWALs-MacBook-Pro.local>
2024-01-19 20:09:24 +0545

    added bloc listeners to show snackbar

--- lib/UI/screens/common_page/register_screen.dart
+++ lib/UI/screens/common_page/register_screen.dart
@@ -21,120 +21,134 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: BlocBuilder<RegistrationBloc, RegistrationState>(
-        builder: (context, state) {
-          if (state is RegistrationLoading) {
-            return const Loader();
-          } else if (state is RegistrationSuccess) {
-            return const LoginPage();
-          } else if (state is RegistrationError) {
-            return const Center(
-              child: Text("Some thing went wrong.."),
-            );
-          } else {
-            return SafeArea(
-              child: SingleChildScrollView(
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 20),
-                      // logo
-                      const Icon(
-                        Icons.app_registration_rounded,
-                        size: 100,
-                      ),
-                      const SizedBox(height: 50),
-                      // Regis
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                          "Join, Jam, and Hit the Stage – Register Today!",
-                          style: TextStyle(
-                            color: Colors.grey[700],
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 25),
-                      // username textfield
-                      MyTextField(
-                        controller: emailController,
-                        hintText: 'Email',
-                        obscureText: false,
-                        formKey: emailFormKey,
-                      ),
-                      const SizedBox(height: 10),
-                      // password textfield
-                      MyTextField(
-                        controller: passwordController,
-                        hintText: 'Password',
-                        obscureText: true,
-                        formKey: passwordFormKey,
-                      ),
-                      const SizedBox(height: 10),
-                      // confirm password field
-                      MyTextField(
-                        controller: confirmPasswordController,
-                        hintText: 'Confirm Password',
-                        obscureText: true,
-                        formKey: confirmPasswordFormKey,
-                      ),
-                      // forgot password?
-                      const SizedBox(height: 25),
-                      // sign Up button
-                      MyButton(
-                        onTap: () {
-                          if (emailFormKey.currentState!.validate() &&
-                              passwordFormKey.currentState!.validate() &&
-                              confirmPasswordFormKey.currentState!.validate()) {
-                            if (passwordController.text !=
-                                confirmPasswordController.text) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('Password do not match...'),
-                                ),
-                              );
-                            } else {
-                              context.read<RegistrationBloc>().add(RegisterUser(
-                                emailController.text,
-                                passwordController.text,
-                              ));
-                            }
-                          }
-                        },
-                        buttonName: "Sign Up",
-                      ),
-                      const SizedBox(height: 50),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Already a member?',
-                            style: TextStyle(color: Colors.grey[700]),
-                          ),
-                          const SizedBox(width: 4),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pushNamed(context, '/login');
-                            },
-                            child: const Text(
-                              'Login here',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
+      body: BlocListener<RegistrationBloc, RegistrationState>(
+        listener: (context, state) {
+          if (state is RegistrationSuccess) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('User Registered Successfully'),
               ),
             );
           }
         },
+        child: BlocBuilder<RegistrationBloc, RegistrationState>(
+          builder: (context, state) {
+            if (state is RegistrationLoading) {
+              return const Loader();
+            } else if (state is RegistrationSuccess) {
+              return const LoginPage();
+            } else if (state is RegistrationError) {
+              return const Center(
+                child: Text("Some thing went wrong.."),
+              );
+            } else {
+              return SafeArea(
+                child: SingleChildScrollView(
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 20),
+                        // logo
+                        const Icon(
+                          Icons.app_registration_rounded,
+                          size: 100,
+                        ),
+                        const SizedBox(height: 50),
+                        // Regis
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "Join, Jam, and Hit the Stage – Register Today!",
+                            style: TextStyle(
+                              color: Colors.grey[700],
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        // username textfield
+                        MyTextField(
+                          controller: emailController,
+                          hintText: 'Email',
+                          obscureText: false,
+                          formKey: emailFormKey,
+                        ),
+                        const SizedBox(height: 10),
+                        // password textfield
+                        MyTextField(
+                          controller: passwordController,
+                          hintText: 'Password',
+                          obscureText: true,
+                          formKey: passwordFormKey,
+                        ),
+                        const SizedBox(height: 10),
+                        // confirm password field
+                        MyTextField(
+                          controller: confirmPasswordController,
+                          hintText: 'Confirm Password',
+                          obscureText: true,
+                          formKey: confirmPasswordFormKey,
+                        ),
+                        // forgot password?
+                        const SizedBox(height: 25),
+                        // sign Up button
+                        MyButton(
+                          onTap: () {
+                            if (emailFormKey.currentState!.validate() &&
+                                passwordFormKey.currentState!.validate() &&
+                                confirmPasswordFormKey.currentState!
+                                    .validate()) {
+                              if (passwordController.text !=
+                                  confirmPasswordController.text) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('Password do not match...'),
+                                  ),
+                                );
+                              } else {
+                                context
+                                    .read<RegistrationBloc>()
+                                    .add(RegisterUser(
+                                      emailController.text,
+                                      passwordController.text,
+                                    ));
+                              }
+                            }
+                          },
+                          buttonName: "Sign Up",
+                        ),
+                        const SizedBox(height: 50),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Already a member?',
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                            const SizedBox(width: 4),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.pushNamed(context, '/login');
+                              },
+                              child: const Text(
+                                'Login here',
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
@@ -149,4 +163,3 @@ class RegisterPage extends StatelessWidget {
 //     confirmPasswordFormKey,
 //     context) {
 //   return
-
