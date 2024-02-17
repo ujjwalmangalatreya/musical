f659e79721746a9f91ce3bfdbe532aa450185ce8
Ujjwal <ujjwalmangalatreya@gmail.com>
2024-02-17 20:14:16 +0545

    Database conecction successful added db.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
 //Import required modules
-const express  = require('express')
+const express = require('express');
+const db = require('./db');
 
-//Create a express application
-const app = express()
+//Create an express application
+const app = express();
 
-// define route handler for the root path
-
-app.get('/',(req,res)=>{
-    res.send('Hello World.');
-})
+// Define route handler for the root path
+app.get('/', (req, res) => {
+    // res.send('Hello World.');
+    db.query('SELECT * FROM users where id =1')
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(err => {
+            res.send("Error: " + err);
+        });
+});
 
 const PORT = process.env.PORT || 3000;
 
-//Start the server
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
-
-
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
