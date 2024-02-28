5b4ee580c2bb8fcd23b2a59044c5b9091d393791
Ujjwal <ujjwalmangalatreya@gmail.com>
2024-02-29 03:45:55 +0545

    clean

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,26 +6,20 @@ const  checkPasswordEmpty =(password)=>{
   return !(password === "" || password === null);
 }
 
-
-
 const checkPasswordValidations = (password) => { 
   if (password.length < 7 || password.length > 15) {
     return false;
   }
-
-  
   const hasLowercase = /[a-z]/.test(password);
   const hasUppercase = /[A-Z]/.test(password);
   const hasNumber = /[0-9]/.test(password);
   if (!hasLowercase || !hasUppercase || !hasNumber) {
     return false;
   }
-
   const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
   if (!hasSpecialChar) {
     return false;
   }
-
   return true;
   
 }
