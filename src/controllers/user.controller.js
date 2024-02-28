35fc13055c373f00f2b1d0033093e3b4a05e1417
Ujjwal <ujjwalmangalatreya@gmail.com>
2024-02-29 03:59:18 +0545

    API Error updated for password validation.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,48 +1,72 @@
-const {sequelize} = require("../db/db_connection.js")
-const { checkUserNameEmpty, checkPasswordEmpty,checkPasswordValidations } = require("../services/user.service.js");
+const { sequelize } = require("../db/db_connection.js");
+const {
+  checkUserNameEmpty,
+  checkPasswordEmpty,
+  checkPasswordValidations,
+} = require("../services/user.service.js");
 const { ApiResponse } = require("../utils/ApiResponse.js");
 const { ApiError } = require("../utils/ApiError.js");
-const Users = require("../models/users.models.js")(sequelize)
+const Users = require("../models/users.models.js")(sequelize);
 
 module.exports = {
   add: async (req, res) => {
     try {
       //Check if username and password empty and null
-      if (!checkUserNameEmpty(req.body.username) ||
-        !checkPasswordEmpty(req.body.password)) {
-        return res.status(400).send(new ApiError(
-          400,
-          "Username and Password should not be empty",
-        ));
+      if (
+        !checkUserNameEmpty(req.body.username) ||
+        !checkPasswordEmpty(req.body.password)
+      ) {
+        return res
+          .status(400)
+          .send(new ApiError(400, "Username and Password should not be empty"));
       } else {
         // add username and password to database Users table
-        const validatePassword = await checkPasswordValidations(req.body.password);
-        
+        const validatePassword = await checkPasswordValidations(
+          req.body.password
+        );
+
         if (validatePassword) {
           const user = await Users.create({
             username: req.body.username,
-            password: req.body.password
+            password: req.body.password,
           });
-        
+
           // Send status code and json response
-          res.status(201).send(new ApiResponse(201, [
-            { username: req.body.username }],
-            "Username Password registered successfully",));
+          res
+            .status(201)
+            .send(
+              new ApiResponse(
+                201,
+                [{ username: req.body.username }],
+                "Username Password registered successfully"
+              )
+            );
         } else {
-          res.send("username password not validated.")
+          res.status(400).send(
+            new ApiError(400, "Username Password cannot be validated", [
+              {
+                PasswordLength:
+                  "Password length must be atleast 7 and not greater than 15 char",
+                PasswordUpperCase:
+                  "Password must have atleast one uppercase character",
+                PasswordLowerCase:
+                  "Password must have atleast one lowercase character",
+                PasswordNumber: "Password must have atleast one number",
+                PasswordSpecialChar:
+                  "Password must have atleast one special character",
+              },
+            ])
+          );
+          return;
         }
       }
     } catch (error) {
       // send if error message
-      res.status(500).send(new ApiError(
-        500,
-        "Internal Server Error",
-        ""+error.toString()
-      ));
+      res
+        .status(500)
+        .send(
+          new ApiError(500, "Internal Server Error", "" + error.toString())
+        );
     }
-  }
-
-  
-  
-  
+  },
 };
