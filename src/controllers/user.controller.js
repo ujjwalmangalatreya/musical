34f594376db24444b46869dee1ff6787bceda32b
Ujjwal <ujjwalmangalatreya@gmail.com>
2024-02-29 03:54:30 +0545

    username password validation completed.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,13 +8,13 @@ module.exports = {
   add: async (req, res) => {
     try {
       //Check if username and password empty and null
-      if(!checkUserNameEmpty(req.body.username) ||
-        !checkPasswordEmpty(req.body.password)){
-            return res.status(400).send(new ApiError(
-              400,
-              "Username and Password should not be empty",
-            ));
-      }else {
+      if (!checkUserNameEmpty(req.body.username) ||
+        !checkPasswordEmpty(req.body.password)) {
+        return res.status(400).send(new ApiError(
+          400,
+          "Username and Password should not be empty",
+        ));
+      } else {
         // add username and password to database Users table
         const validatePassword = await checkPasswordValidations(req.body.password);
         
@@ -23,12 +23,15 @@ module.exports = {
             username: req.body.username,
             password: req.body.password
           });
+        
+          // Send status code and json response
+          res.status(201).send(new ApiResponse(201, [
+            { username: req.body.username }],
+            "Username Password registered successfully",));
+        } else {
+          res.send("username password not validated.")
         }
-        // Send status code and json response
-        res.status(201).send(new ApiResponse(201,[
-          {username : req.body.username}],
-          "Username Password registered successfully",));
-        }
+      }
     } catch (error) {
       // send if error message
       res.status(500).send(new ApiError(
